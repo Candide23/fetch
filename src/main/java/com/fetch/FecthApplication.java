95e2434b07ae630689035c2467086d650617866f
candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-02-24 13:44:32 -0600

    first commit

---
+++ src/main/java/com/fetch/FecthApplication.java
@@ -0,0 +1,13 @@
+package com.fetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FecthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FecthApplication.class, args);
+	}
+
+}
